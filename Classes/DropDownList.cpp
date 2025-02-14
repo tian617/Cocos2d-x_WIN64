3fe629b58071a77f23af028183adac83714c8771
christtang <christtang@tencent.com>
2025-02-14 16:02:09 +0800

    add MenuItemLabelWithBg

--- Classes/DropDownList.cpp
+++ Classes/DropDownList.cpp
@@ -1,5 +1,149 @@
 // DropDownList.cpp
 #include "DropDownList.h"
+#include <2d/CCMenuItem.cpp>
+
+//
+//CCMenuItemLabel
+//
+
+void MenuItemLabelWithBg::setLabel(Node* var)
+{
+	if (var)
+	{
+		var->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
+        Size spBgSize = _spriteBg->getContentSize();
+        var->setPosition(Vec2(0, spBgSize.height / 2));
+		setContentSize(spBgSize);
+		addChild(var, 1);
+	}
+
+	if (_label)
+	{
+		removeChild(_label, true);
+	}
+
+	_label = var;
+}
+
+MenuItemLabelWithBg* MenuItemLabelWithBg::create(Node* label, Node* sprite, const ccMenuCallback& callback)
+{
+	MenuItemLabelWithBg* ret = new (std::nothrow) MenuItemLabelWithBg();
+	ret->initWithLabel(label, sprite, callback);
+	ret->autorelease();
+	return ret;
+}
+
+MenuItemLabelWithBg* MenuItemLabelWithBg::create(Node* label)
+{
+	MenuItemLabelWithBg* ret = new (std::nothrow) MenuItemLabelWithBg();
+	ret->initWithLabel(label, nullptr, (const ccMenuCallback&) nullptr);
+	ret->autorelease();
+	return ret;
+}
+
+
+bool MenuItemLabelWithBg::initWithLabel(Node* label, Node* sprite, const ccMenuCallback& callback)
+{
+	MenuItem::initWithCallback(callback);
+	_originalScale = 1.0f;
+	_colorBackup = Color3B::WHITE;
+	setDisabledColor(Color3B(126, 126, 126));
+	_spriteBg = sprite;
+	this->setLabel(label);
+
+    label->setContentSize(_spriteBg->getContentSize());
+    _spriteBg->setPosition(Vec2::ZERO);
+    _spriteBg->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
+    addChild(_spriteBg);  // add bg
+
+	setCascadeColorEnabled(true);
+	setCascadeOpacityEnabled(true);
+
+	return true;
+}
+
+
+MenuItemLabelWithBg::~MenuItemLabelWithBg()
+{
+}
+
+void MenuItemLabelWithBg::setString(const std::string& label)
+{
+	dynamic_cast<LabelProtocol*>(_label)->setString(label);
+
+    this->setContentSize(_spriteBg->getContentSize());
+	//this->setContentSize(_label->getContentSize());
+}
+
+std::string MenuItemLabelWithBg::getString() const
+{
+	auto label = dynamic_cast<LabelProtocol*>(_label);
+	return label->getString();
+}
+
+void MenuItemLabelWithBg::activate()
+{
+	if (_enabled)
+	{
+		this->stopAllActions();
+		this->setScale(_originalScale);
+		MenuItem::activate();
+	}
+}
+
+void MenuItemLabelWithBg::selected()
+{
+	// subclass to change the default action
+	if (_enabled)
+	{
+		MenuItem::selected();
+
+		Action* action = getActionByTag(kZoomActionTag);
+		if (action)
+		{
+			this->stopAction(action);
+		}
+		else
+		{
+			_originalScale = this->getScale();
+		}
+
+		Action* zoomAction = ScaleTo::create(0.1f, _originalScale * 1.05f);
+		zoomAction->setTag(kZoomActionTag);
+		this->runAction(zoomAction);
+	}
+}
+
+void MenuItemLabelWithBg::unselected()
+{
+	// subclass to change the default action
+	if (_enabled)
+	{
+		MenuItem::unselected();
+		this->stopActionByTag(kZoomActionTag);
+		Action* zoomAction = ScaleTo::create(0.1f, _originalScale);
+		zoomAction->setTag(kZoomActionTag);
+		this->runAction(zoomAction);
+	}
+}
+
+void MenuItemLabelWithBg::setEnabled(bool enabled)
+{
+	if (_enabled != enabled)
+	{
+		if (enabled == false)
+		{
+			_colorBackup = this->getColor();
+			this->setColor(_disabledColor);
+		}
+		else
+		{
+			this->setColor(_colorBackup);
+		}
+	}
+	MenuItem::setEnabled(enabled);
+}
+
 
 DropDownList* DropDownList::create(const std::string& defaultText, const std::vector<std::string>& options) {
     DropDownList* pRet = new (std::nothrow) DropDownList();
@@ -52,18 +196,13 @@ bool DropDownList::init(const std::string& defaultText, const std::vector<std::s
 
     for (size_t i = 0; i < options.size(); ++i) {
         auto label = Label::createWithSystemFont(options[i], "Arial", 12);
-        auto menuItem = MenuItemLabel::create(label, CC_CALLBACK_1(DropDownList::onOptionClicked, this));
+        Sprite* bgSpr = Sprite::create();
+        bgSpr->initWithFile("btn_normal.png");
+        bgSpr->setContentSize(Size(width, height));
+        auto menuItem = MenuItemLabelWithBg::create(label, bgSpr, CC_CALLBACK_1(DropDownList::onOptionClicked, this));
         menuItem->setTag(i);
         _optionItems.push_back(menuItem);
 
-
-		LayerColor* _bgColor = LayerColor::create(Color4B(200, 200, 200, 66));
-		_bgColor->setPosition(0, 0);
-		_bgColor->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
-		_bgColor->setContentSize(Size(width, height - 2));
-		menuItem->addChild(_bgColor, -1);
-
-        menuItem->setContentSize(Size(width, height));
         menuItem->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
         menuItem->setPosition(Vec2(width/2, height * (i + 1)));
 
@@ -110,7 +249,7 @@ void DropDownList::setSelectedIndex(int index) {
 }
 
 void DropDownList::onOptionClicked(Ref* sender) {
-    auto menuItem = dynamic_cast<MenuItemLabel*>(sender);
+    auto menuItem = dynamic_cast<MenuItemLabelWithBg*>(sender);
     if (menuItem) {
         _selectedIndex = menuItem->getTag();
         _label->setString(menuItem->getString());
@@ -125,3 +264,5 @@ void DropDownList::toggleOptions() {
         item->setVisible(!isVisible);
     }
 }
+
+
--- Classes/DropDownList.h
+++ Classes/DropDownList.h
@@ -9,6 +9,71 @@ USING_NS_CC;
 using namespace cocos2d::ui;
 
 
+class MenuItemLabelWithBg : public MenuItem
+{
+public:
+
+    /** Creates a MenuItemLabelWithBg with a Label and a callback. */
+    static MenuItemLabelWithBg* create(Node* label, Node* sprite, const ccMenuCallback& callback);
+
+    /** Creates a MenuItemLabelWithBg with a Label. Target and selector will be nil. */
+    static MenuItemLabelWithBg* create(Node* label);
+
+    /** Sets a new string to the inner label. */
+    void setString(const std::string& label);
+
+    /** Get the inner string of the inner label. */
+    std::string getString() const;
+
+    /** Gets the color that will be used when the item is disabled. */
+    const Color3B& getDisabledColor() const { return _disabledColor; }
+
+    /** Sets the color that will be used when the item is disabled. */
+    void setDisabledColor(const Color3B& color) { _disabledColor = color; }
+
+    /** Gets the label that is rendered. */
+    Node* getLabel() const { return _label; }
+
+    /** Sets the label that is rendered. */
+    void setLabel(Node* node);
+
+    // Overrides
+    virtual void activate() override;
+    virtual void selected() override;
+    virtual void unselected() override;
+    virtual void setEnabled(bool enabled) override;
+
+CC_CONSTRUCTOR_ACCESS:
+    /**
+     * @js ctor
+     */
+    MenuItemLabelWithBg()
+        : _originalScale(0.0)
+        , _label(nullptr)
+    {}
+    /**
+     * @js NA
+     * @lua NA
+     */
+    virtual ~MenuItemLabelWithBg();
+
+    /** Initializes a MenuItemLabelWithBg with a Label, target and selector. */
+    bool initWithLabel(Node* label, Node* sprite, const ccMenuCallback& callback);
+
+protected:
+    Color3B    _colorBackup;
+    float      _originalScale;
+
+    /** The color that will be used to disable the item. */
+    Color3B _disabledColor;
+    /** Label that is rendered. It can be any Node that implements the LabelProtocol. */
+    Node* _label;
+    Node* _spriteBg;
+
+private:
+    CC_DISALLOW_COPY_AND_ASSIGN(MenuItemLabelWithBg);
+};
+
 class DropDownList : public Widget {
 public:
     static DropDownList* create(const std::string& defaultText, const std::vector<std::string>& options);
@@ -29,7 +94,7 @@ private:
     Label* _label;
     Sprite* _arrow;
     Menu* _menu;
-    std::vector<MenuItemLabel*> _optionItems;
+    std::vector<MenuItemLabelWithBg*> _optionItems;
     int _selectedIndex;
 };
 
